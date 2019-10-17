912ef47502674cade4232a98f4fd22f61454dbb1
Mateusz Urbanek <mateuszurbanek102@gmail.com>
2019-10-17 14:13:34 +0200

    Add files via upload

---
+++ ZadaniePetle.java
@@ -0,0 +1,21 @@
+public class ZadaniePetle {
+    public static void main(String[] args) {
+
+        //Wyświetl liczby od 1 do 10
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);
+
+        }
+        System.out.println("----------");
+        //Wyświetl liczby od 5 do 10
+        for (int i = 5; i < 11; i++){
+            System.out.println(i);
+        }
+        System.out.println("----------");
+        //Wyświetl liczby od 10 do 1
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println("----------");
+    }
+}
