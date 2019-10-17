4cb6d6ea3486d9b3f033727358a754c5c5929ee1
Mateusz Urbanek <mateuszurbanek102@gmail.com>
2019-10-17 14:32:18 +0200

    Add files via upload

---
+++ ExFiveLoops.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ExFiveLoops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę: ");
+        int firstNum = input.nextInt();
+
+        System.out.println("Podaj drugą liczbę: ");
+        int secondNum = input.nextInt();
+
+        if (firstNum < secondNum){
+            for (int i = firstNum; i < secondNum; i++){
+                System.out.println(i);
+            }
+            } else if (firstNum > secondNum || firstNum == secondNum){
+                System.out.println("Żle podane parametry");
+            }
+    }
+}
